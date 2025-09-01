3ef523676d16256479fa16bb2925929300c5fdb7
suvitcha <boom.suvitcha@gmail.com>
2025-09-01 15:41:11 +0700

    edit-cors-server

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
-    "https://app-note-frontend.vercel.app/",
+    "https://app-note-frontend.vercel.app",
   ], // frontend domain
   credentials: true, // ✅ allow cookies to be sent
 };
